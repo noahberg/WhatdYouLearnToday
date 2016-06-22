2d1ae454ebd6e01baafc7641f01f44cff34e4523
Noah Bergman <nabergma@usc.edu>
2016-06-22 01:48:04 -0700

    Initial Commit

---
+++ What'd You Learn TodayTests/What_d_You_Learn_TodayTests.swift
@@ -0,0 +1,36 @@
+//
+//  What_d_You_Learn_TodayTests.swift
+//  What'd You Learn TodayTests
+//
+//  Created by Noah Bergman on 6/22/16.
+//  Copyright © 2016 Noah Bergman. All rights reserved.
+//
+
+import XCTest
+@testable import What_d_You_Learn_Today
+
+class What_d_You_Learn_TodayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
